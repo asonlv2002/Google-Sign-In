ca0e420979f0383f1041b898455b2ad85057c0bf
Võ Huỳnh Sơn <sonvh@droidgames.vn>
2024-11-25 18:59:46 +0700

    login

---
+++ Assets/Scenes/Login.cs
@@ -0,0 +1,56 @@
+﻿using Google;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+    string webClientID = "296351000649-3pc46c85crce5imm9sulfofk37qq0d7p.apps.googleusercontent.com";
+    private GoogleSignInConfiguration config;
+    private void Awake()
+    {
+        config = new GoogleSignInConfiguration()
+        {
+            WebClientId = webClientID,
+            RequestIdToken = true,
+            UseGameSignIn = false,
+            RequestEmail = true,
+        };
+    }
+
+    public async void LoginGoogle()
+    {
+        GoogleSignIn.Configuration = config;
+        await GoogleSignIn.DefaultInstance.SignIn().ContinueWith(OnAuthenticationFinished, TaskScheduler.Default);
+    }
+
+    private void OnAuthenticationFinished(Task<GoogleSignInUser> task)
+    {
+        if (task.IsFaulted)
+        {
+            using (var enumerator = task.Exception.InnerExceptions.GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                {
+                    GoogleSignIn.SignInException error = (GoogleSignIn.SignInException)enumerator.Current;
+                    Debug.LogError("Google Sign-In failed: " + error.Status + " " + error.Message);
+
+                    // Kiểm tra nếu lỗi là do người dùng hủy
+                    if (error.Status == GoogleSignInStatusCode.CANCELED)
+                    {
+                        Debug.LogWarning("Sign-In was canceled by the user.");
+                    }
+                }
+            }
+        }
+        else if (task.IsCanceled)
+        {
+            Debug.LogWarning("Google Sign-In was canceled.");
+        }
+        else
+        {
+            Debug.Log("Welcome: " + task.Result.DisplayName + "!");
+            Debug.Log("Email: " + task.Result.Email);
+            Debug.Log("IdToken: " + task.Result.IdToken);
+        }
+    }
+}
